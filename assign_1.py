a808abe6b591d35d76431f999d522acdd4dc91df
pkambali <36163189+pkambali@users.noreply.github.com>
2019-10-21 10:48:39 +0530

    assign 1

---
+++ assign_1.py
@@ -0,0 +1,19 @@
+def pyramid(i):
+    for j in range(i):
+        for k in range(j, i):
+            print(" ", end ="")
+        if j>1:
+            for l in range(j):
+                print(l+1, end =" ")
+            rev = j-1
+            while rev>0:
+                print(rev, end =" ")
+                rev -= 1
+
+        else:   
+             for l in range(j+1):
+                print(l+1, end =" ")
+
+        print("\n", end ="")
+
+pyramid(5)
